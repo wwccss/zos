5f9b02fb10ec5a7f22df2e60b57e8da0e7fc363d
wangchunsheng <chunsheng@cnezsoft.com>
2013-07-12 05:29:22 +0000

    + rebuild php and add gd support to it.

--- 02_blfs/05_php/08_php/02_build_php.sh
+++ 02_blfs/05_php/08_php/02_build_php.sh
@@ -1,8 +1,8 @@
 cd /sources/blfs
-tar xvf php*.tar.bz2
+#tar xvf php*.tar.bz2
 cd php*
 
-./configure --prefix=/usr \
+#./configure --prefix=/usr \
             --sysconfdir=/etc \
             --with-apxs2 \
             --with-config-file-path=/etc \
@@ -28,5 +28,24 @@ cd php*
             --with-iconv \
 	    --with-mhash \
             --with-mcrypt \
-	    --disable-cgi
-make
+	    --disable-cgi \
+            --with-gd \
+            --with-jpeg-dir=/usr/ \
+            --with-png-dir=/usr/ \
+            --with-zlib-dir=/usr/ \
+            --with-freetype-dir=/usr/ \
+            --enable-gd-native-ttf \
+            --enable-gd-jis-conv 
+#make
+
+sudo make install &&
+sudo install -v -m644 php.ini-production /etc/php.ini &&
+
+sudo install -v -m755 -d /usr/share/doc/php-5.4.11 &&
+sudo install -v -m644    CODING_STANDARDS EXTENSIONS INSTALL NEWS \
+                    README* UPGRADING* php.gif \
+                    /usr/share/doc/php-5.4.11 &&
+sudo ln -v -sfn          /usr/lib/php/doc/Archive_Tar/docs/Archive_Tar.txt \
+                    /usr/share/doc/php-5.4.11 &&
+sudo ln -v -sfn          /usr/lib/php/doc/Structures_Graph/docs \
+                    /usr/share/doc/php-5.4.11
