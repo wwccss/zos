db48648cec95de544489032e0ef4a2bee4f129a7
wangchunsheng <chunsheng@cnezsoft.com>
2013-07-13 10:11:40 +0000

    * fix the error.

--- 04_release/07_install_grub.sh
+++ 04_release/07_install_grub.sh
@@ -5,5 +5,5 @@ sudo mkdir /mnt/lfs/tools/
 sudo mount /dev/sdc1 /mnt/lfs/tools
 sudo mount /dev/sdd1 /mnt/lfs/sources
 
-echo "now will enter the chroot env, after that, run grub-install /dev/sde\n\n"
+echo "now will enter the chroot env, after that, run grub-install /dev/sdf\n\n"
 bash ../01_lfs/07/7.00_chroot.sh
