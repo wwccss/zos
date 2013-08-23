cd27534a708b230d181d08895df9a5342c2e2c8b
root <root@zentao.zentao>
2013-08-23 18:28:01 +0800

    * upgrade the blfs_boot_scripts.

--- 02_blfs/01_basic/1.1_prepare/02_down_blfs_boot_scripts.sh
+++ 02_blfs/01_basic/1.1_prepare/02_down_blfs_boot_scripts.sh
@@ -1,5 +1,5 @@
 sudo mkdir $LFS/sources/blfs
 sudo chown lfs:lfs $LFS/sources/blfs
 cd $LFS/sources/blfs
-axel http://www.linuxfromscratch.org/blfs/downloads/svn/blfs-bootscripts-20130611.tar.bz2
+wget http://www.linuxfromscratch.org/blfs/downloads/svn/blfs-bootscripts-20130725.tar.bz2
 tar xvf blfs-bootscripts*.tar.bz2
