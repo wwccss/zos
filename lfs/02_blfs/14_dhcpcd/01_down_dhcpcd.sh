cf3c986d5deb301df7ef1a1930ce6a511772ee96
wangchunsheng <chunsheng@cnezsoft.com>
2013-08-24 20:02:04 +0800

    + add the shell for dhcpcd.

---
+++ lfs/02_blfs/14_dhcpcd/01_down_dhcpcd.sh
@@ -0,0 +1,2 @@
+cd /sources/blfs
+wget "http://roy.marples.name/downloads/dhcpcd/dhcpcd-6.0.5.tar.bz2"
---
+++ lfs/02_blfs/14_dhcpcd/02_install_dhcpcd.sh
@@ -0,0 +1,27 @@
+cd /sources/blfs
+tar xvf dhcpcd*.tar.bz2
+cd dhcp*
+
+./configure --libexecdir=/lib/dhcpcd \
+            --dbdir=/run             \
+            --sysconfdir=/etc &&
+make
+
+sudo make install
+
+sudo sed -i "s;/var/lib;/run;g" dhcpcd-hooks/50-dhcpcd-compat &&
+sudo install -v -m 644 dhcpcd-hooks/50-dhcpcd-compat /lib/dhcpcd/dhcpcd-hooks/
+
+cd ../blfs-boot*
+sudo make install-service-dhcpcd
+
+cat > ifconfig << "EOF"
+ONBOOT="yes"
+IFACE="eth0"
+SERVICE="dhcpcd"
+DHCP_START="-b -q"
+DHCP_STOP="-k"
+EOF
+
+ifconfig=`ls /etc/sysconfig/ifconfig*`
+sudo mv ifconfig $ifconfig
