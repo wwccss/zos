497233639a8595f695336325e7a931ffb4fd3c45
wwccss <chunsheng@cnezsoft.com>
2014-09-11 02:23:01 +0800

    * upgrade to 7.6.rc1.

--- lfs/01_lfs/05/5.15_build_ncurses.sh
+++ lfs/01_lfs/05/5.15_build_ncurses.sh
@@ -2,7 +2,11 @@ cd $LFS/sources
 tar zxvf ncurses*
 cd ncurses*
 
-./configure --prefix=/tools --with-shared \
-      --without-debug --without-ada --enable-overwrite
+./configure --prefix=/tools \
+            --with-shared   \
+            --without-debug \
+            --without-ada   \
+            --enable-widec  \
+            --enable-overwrite
 
 make && make install
