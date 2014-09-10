03e9b252a05ad5fed1a65c958bc279c47033e1c3
wwccss <chunsheng@cnezsoft.com>
2014-09-10 21:10:13 +0800

    + add the shell to build libstdc.

---
+++ lfs/01_lfs/05/5.08_build_libstdc.sh
@@ -0,0 +1,17 @@
+cd $LFS/sources/gcc*
+
+mkdir -pv ../gcc-build
+cd ../gcc-build
+
+../gcc-4.9.1/libstdc++-v3/configure \
+    --host=$LFS_TGT                 \
+    --prefix=/tools                 \
+    --disable-multilib              \
+    --disable-shared                \
+    --disable-nls                   \
+    --disable-libstdcxx-threads     \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/4.9.1
+
+make 
+make install
