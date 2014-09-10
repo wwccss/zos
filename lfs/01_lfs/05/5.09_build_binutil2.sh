0f0f5066c73d5b7d496f612001384537c09a8435
wwccss <chunsheng@cnezsoft.com>
2014-09-10 21:17:52 +0800

    * upgrade 7.6.rc1.

--- lfs/01_lfs/05/5.09_build_binutil2.sh
+++ lfs/01_lfs/05/5.09_build_binutil2.sh
@@ -1,20 +1,20 @@
 cd $LFS/sources
 
-tar jxvf binutils-2.23.1.tar.bz2
+tar jxvf binutils-2.24.tar.bz2
 cd binutils*
 
 rm -fr ../binutils-build
 mkdir -v ../binutils-build
 cd ../binutils-build
 
-CC=$LFS_TGT-gcc            \
-AR=$LFS_TGT-ar             \
-RANLIB=$LFS_TGT-ranlib     \
+CC=$LFS_TGT-gcc                \
+AR=$LFS_TGT-ar                 \
+RANLIB=$LFS_TGT-ranlib         \
 
-# see http://listarc.com/showthread.php?4393817-5.13.+Check-0.9.9 to fix the error when build check in 5.13
-../binutils-2.23.1/configure \
-    --prefix=/tools        \
-    --disable-nls          \
+../binutils-2.24/configure     \
+    --prefix=/tools            \
+    --disable-nls              \
+    --disable-werror           \
     --with-lib-path=/tools/lib \
     --with-sysroot
 
