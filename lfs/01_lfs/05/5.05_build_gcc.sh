63680b7fea1039925736cd4719de580ecaa43049
wwccss <chunsheng@cnezsoft.com>
2014-09-10 20:42:18 +0800

    * upgrade to 7.6.1.

--- lfs/01_lfs/05/5.05_build_gcc.sh
+++ lfs/01_lfs/05/5.05_build_gcc.sh
@@ -1,13 +1,13 @@
 cd $LFS/sources
-tar jxvf gcc*
+tar xvf gcc*.bz2
 cd gcc*
 
-tar -Jxf ../mpfr-3.1.1.tar.xz
-mv -v mpfr-3.1.1 mpfr
-tar -Jxf ../gmp-5.1.1.tar.xz
-mv -v gmp-5.1.1 gmp
-tar -zxf ../mpc-1.0.1.tar.gz
-mv -v mpc-1.0.1 mpc
+tar -xf ../mpfr-3.1.2.tar.xz
+mv -v mpfr-3.1.2 mpfr
+tar -xf ../gmp-6.0.0a.tar.xz
+mv -v gmp-6.0.0 gmp
+tar -xf ../mpc-1.0.2.tar.gz
+mv -v mpc-1.0.2 mpc
 
 for file in \
  $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
@@ -24,33 +24,34 @@ do
 done
 
 sed -i '/k prot/agcc_cv_libc_provides_ssp=yes' gcc/configure
-sed -i 's/BUILD_INFO=info/BUILD_INFO=/' gcc/configure
+sed -i 's/if \((code.*))\)/if (\1 \&\& \!DEBUG_INSN_P (insn))/' gcc/sched-deps.c
 
 mkdir -v ../gcc-build
 cd ../gcc-build
 
-../gcc-4.7.2/configure         \
-    --target=$LFS_TGT          \
-    --prefix=/tools            \
-    --with-sysroot=$LFS        \
-    --with-newlib              \
-    --without-headers          \
-    --with-local-prefix=/tools \
-    --with-native-system-header-dir=/tools/include \
-    --disable-nls              \
-    --disable-shared           \
-    --disable-multilib         \
-    --disable-decimal-float    \
-    --disable-threads          \
-    --disable-libmudflap       \
-    --disable-libssp           \
-    --disable-libgomp          \
-    --disable-libquadmath      \
-    --enable-languages=c       \
-    --with-mpfr-include=$(pwd)/../gcc-4.7.2/mpfr/src \
-    --with-mpfr-lib=$(pwd)/mpfr/src/.libs
+../gcc-4.9.1/configure                               \
+    --target=$LFS_TGT                                \
+    --prefix=/tools                                  \
+    --with-sysroot=$LFS                              \
+    --with-newlib                                    \
+    --without-headers                                \
+    --with-local-prefix=/tools                       \
+    --with-native-system-header-dir=/tools/include   \
+    --disable-nls                                    \
+    --disable-shared                                 \
+    --disable-multilib                               \
+    --disable-decimal-float                          \
+    --disable-threads                                \
+    --disable-libatomic                              \
+    --disable-libgomp                                \
+    --disable-libitm                                 \
+    --disable-libquadmath                            \
+    --disable-libsanitizer                           \
+    --disable-libssp                                 \
+    --disable-libvtv                                 \
+    --disable-libcilkrts                             \
+    --disable-libstdc++-v3                           \
+    --enable-languages=c,c++
 
 make
 make install
-
-ln -sv libgcc.a `$LFS_TGT-gcc -print-libgcc-file-name | sed 's/libgcc/&_eh/'`
