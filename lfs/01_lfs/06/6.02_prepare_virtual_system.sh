0482c4fc28a31c1ffb1399213324b06c66d14442
wwccss <chunsheng@cnezsoft.com>
2014-09-17 15:56:01 +0800

    * upgrade to 7.6.rc1.

--- lfs/01_lfs/06/6.02_prepare_virtual_system.sh
+++ lfs/01_lfs/06/6.02_prepare_virtual_system.sh
@@ -1,17 +1,13 @@
-sudo ln -s ~/lfs/lfs $LFS/cmd
-sudo mkdir -v $LFS/{dev,proc,sys}   
+sudo ln -s ~/lfs/zentaomos $LFS/zentaomos
+
+sudo mkdir -pv $LFS/{dev,proc,sys,run}
 sudo mknod -m 600 $LFS/dev/console c 5 1
 sudo mknod -m 666 $LFS/dev/null c 1 3
 sudo mount -v --bind /dev $LFS/dev
-sudo mount -vt devpts devpts $LFS/dev/pts
+sudo mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
 sudo mount -vt proc proc $LFS/proc
 sudo mount -vt sysfs sysfs $LFS/sys
-
+sudo mount -vt tmpfs tmpfs $LFS/run
 if [ -h $LFS/dev/shm ]; then
-  link=$(readlink $LFS/dev/shm)
-  sudo mkdir -p $LFS/$link
-  sudo mount -vt tmpfs shm $LFS/$link
-  unset link
-else
-  sudo mount -vt tmpfs shm $LFS/dev/shm
+  sudo mkdir -pv $LFS/$(readlink $LFS/dev/shm)
 fi
