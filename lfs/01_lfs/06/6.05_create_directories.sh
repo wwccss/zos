c99d9a1b604d9127893a1baa2b70b289734d4030
wwccss <chunsheng@cnezsoft.com>
2014-09-17 19:15:41 +0800

    * upgrade to 7.6.rc1.

--- lfs/01_lfs/06/6.05_create_directories.sh
+++ lfs/01_lfs/06/6.05_create_directories.sh
@@ -1,18 +1,20 @@
-mkdir -pv /{bin,boot,etc/{opt,sysconfig},home,lib,mnt,opt,run}
+mkdir -pv /{bin,boot,etc/{opt,sysconfig},home,lib,mnt,opt}
 mkdir -pv /{media/{floppy,cdrom},sbin,srv,var}
 install -dv -m 0750 /root
 install -dv -m 1777 /tmp /var/tmp
 mkdir -pv /usr/{,local/}{bin,include,lib,sbin,src}
-mkdir -pv /usr/{,local/}share/{doc,info,locale,man}
+mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
 mkdir -v  /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -v  /usr/libexec
 mkdir -pv /usr/{,local/}share/man/man{1..8}
-for dir in /usr /usr/local; do
-  ln -sv share/{man,doc,info} $dir
-done
+
 case $(uname -m) in
- x86_64) ln -sv lib /lib64 && ln -sv lib /usr/lib64 ;;
+ x86_64) ln -sv lib /lib64
+         ln -sv lib /usr/lib64
+         ln -sv lib /usr/local/lib64 ;;
 esac
+
 mkdir -v /var/{log,mail,spool}
 ln -sv /run /var/run
 ln -sv /run/lock /var/lock
-mkdir -pv /var/{opt,cache,lib/{misc,locate},local}
+mkdir -pv /var/{opt,cache,lib/{color,misc,locate},local}
