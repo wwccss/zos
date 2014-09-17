212a98348e21c9bcde9ba7e13609e411e3874396
wwccss <chunsheng@cnezsoft.com>
2014-09-18 05:53:59 +0800

    * add the logic to build packages.

--- lfs/01_lfs/06/6.07_build_linux_api_headers.sh
+++ lfs/01_lfs/06/6.07_build_linux_api_headers.sh
@@ -4,6 +4,8 @@ cd linux*
 
 make mrproper
 
+pkg watch /mnt/lfs
 make INSTALL_HDR_PATH=dest headers_install
 find dest/include \( -name .install -o -name ..install.cmd \) -delete
 cp -rv dest/include/* /usr/include
+pkg build /sources/ini/linux.ini winst.log /
