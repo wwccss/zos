8e89067ea57b59cb5b95e173b82601d21a6ca927
wwccss <chunsheng@cnezsoft.com>
2014-09-17 19:16:18 +0800

    * upgrade to 7.6.rc.1.

--- lfs/01_lfs/06/6.06_create_essential_files.sh
+++ lfs/01_lfs/06/6.06_create_essential_files.sh
@@ -1,21 +1,28 @@
 ln -sv /tools/bin/{bash,cat,echo,pwd,stty} /bin
 ln -sv /tools/bin/perl /usr/bin
+ln -sv /tools/bin/env /usr/bin
 ln -sv /tools/lib/libgcc_s.so{,.1} /usr/lib
 ln -sv /tools/lib/libstdc++.so{,.6} /usr/lib
 sed 's/tools/usr/' /tools/lib/libstdc++.la > /usr/lib/libstdc++.la
 ln -sv bash /bin/sh
 
-touch /etc/mtab
+# copy pkg files.
+ln -s /tools/zentaomos /root/
+ln -s /tools/zentaomos/pkg/pkg /tools/bin/
+
+ln -sv /proc/self/mounts /etc/mtab
 
 cat > /etc/passwd << "EOF"
 root:x:0:0:root:/root:/bin/bash
 bin:x:1:1:bin:/dev/null:/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
 nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
 EOF
 
 cat > /etc/group << "EOF"
 root:x:0:
-bin:x:1:
+bin:x:1:daemon
 sys:x:2:
 kmem:x:3:
 tape:x:4:
@@ -30,9 +37,13 @@ video:x:12:
 utmp:x:13:
 usb:x:14:
 cdrom:x:15:
-sudo:x:27:
+adm:x:16:
+messagebus:x:18:
+systemd-journal:x:23:
+input:x:24:
 mail:x:34:
 nogroup:x:99:
+users:x:999:
 EOF
 
 touch /var/log/{btmp,lastlog,wtmp}
@@ -40,4 +51,4 @@ chgrp -v utmp /var/log/lastlog
 chmod -v 664  /var/log/lastlog
 chmod -v 600  /var/log/btmp
 
-/tools/bin/bash --login +h
+exec /tools/bin/bash --login +h
