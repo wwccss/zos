13b26a091506e22d1ad1081ec9977a3e2daaa049
wwccss <chunsheng@cnezsoft.com>
2014-09-17 19:17:32 +0800

    * upgrae to 7.6.rc1.

--- lfs/01_lfs/06/6.07_build_linux_api_headers.sh
+++ lfs/01_lfs/06/6.07_build_linux_api_headers.sh
@@ -4,7 +4,6 @@ cd linux*
 
 make mrproper
 
-make headers_check
 make INSTALL_HDR_PATH=dest headers_install
 find dest/include \( -name .install -o -name ..install.cmd \) -delete
 cp -rv dest/include/* /usr/include
