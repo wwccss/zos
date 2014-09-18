4f9c56076b17d492e470a9a3534fb02335225970
wwccss <chunsheng@cnezsoft.com>
2014-09-18 12:35:26 +0800

    * upgrade to 7.6.rc.1.

--- lfs/01_lfs/06/6.10.1_adjust_tool_chain.sh
+++ lfs/01_lfs/06/6.10.1_adjust_tool_chain.sh
@@ -3,7 +3,7 @@ mv -v /tools/$(gcc -dumpmachine)/bin/{ld,ld-old}
 mv -v /tools/bin/{ld-new,ld}
 ln -sv /tools/bin/ld /tools/$(gcc -dumpmachine)/bin/ld
 
-gcc -dumpspecs | sed -e 's@/tools@@g' \
+gcc -dumpspecs | sed -e 's@/tools@@g'                   \
     -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
-    -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' > \
+    -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >      \
     `dirname $(gcc --print-libgcc-file-name)`/specs
