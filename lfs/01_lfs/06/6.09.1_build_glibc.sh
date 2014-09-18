48c8c1692b9cec97b071ac7146c38fc4c81e6da2
wwccss <chunsheng@cnezsoft.com>
2014-09-18 08:21:31 +0800

    * upgrade to 7.6.rc1.

--- lfs/01_lfs/06/6.09.1_build_glibc.sh
+++ lfs/01_lfs/06/6.09.1_build_glibc.sh
@@ -2,12 +2,14 @@ cd /sources/
 tar xvf glibc*.tar.xz
 cd glibc*
 
+patch -Np1 -i ../glibc-2.20-fhs-1.patch
+
 mkdir -v ../glibc-build
 cd ../glibc-build
 
-../glibc-2.17/configure  \
+../glibc-2.20/configure    \
     --prefix=/usr          \
     --disable-profile      \
-    --enable-kernel=2.6.25 \
-    --libexecdir=/usr/lib/glibc
+    --enable-kernel=2.6.32 \
+    --enable-obsolete-rpc
 make
