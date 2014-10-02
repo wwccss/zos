c88c9d70c4f83e678783d2074b4412e0f3dae7b2
wwccss <chunsheng@cnezsoft.com>
2014-10-02 18:08:42 +0800

    * upgrade to 7.6.

--- lfs/01_lfs/06/6.16_build_mpc.sh
+++ lfs/01_lfs/06/6.16_build_mpc.sh
@@ -2,8 +2,14 @@ cd /sources/
 tar xvf mpc*.tar.gz
 cd mpc*
 
-./configure  --prefix=/usr
+pkg watch /mnt/lfs
+
+./configure --prefix=/usr --docdir=/usr/share/doc/mpc-1.0.2
 
 make
+make html
 make check
 make install
+make install-html
+
+pkg build /sources/ini/mpc.ini winst.log /
