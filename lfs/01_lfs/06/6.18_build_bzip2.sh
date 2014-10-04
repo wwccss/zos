1e834716042628596099b6643b175f34ad061c95
wwccss <chunsheng@cnezsoft.com>
2014-10-04 09:01:31 +0800

    * rename them.

--- lfs/01_lfs/06/6.18_build_bzip2.sh
+++ lfs/01_lfs/06/6.18_build_bzip2.sh
@@ -1,6 +1,9 @@
 cd /sources/
 tar xvf bzip*.tar.gz
 cd bzip2*
+
+pkg watch /mnt/lfs
+
 patch -Np1 -i ../bzip2-1.0.6-install_docs-1.patch
 sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
 sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
@@ -8,9 +11,12 @@ make -f Makefile-libbz2_so
 make clean
 make
 make PREFIX=/usr install
+
 cp -v bzip2-shared /bin/bzip2
 cp -av libbz2.so* /lib
 ln -sv ../../lib/libbz2.so.1.0 /usr/lib/libbz2.so
 rm -v /usr/bin/{bunzip2,bzcat,bzip2}
 ln -sv bzip2 /bin/bunzip2
 ln -sv bzip2 /bin/bzcat
+
+pkg build /sources/ini/bzip2.ini winst.log /
