586f4218db46786dfda2eee66bd0845dd691fe05
wwccss <chunsheng@cnezsoft.com>
2014-10-04 09:17:14 +0800

    + add attr shell.

---
+++ lfs/01_lfs/06/6.21_build_atrr.sh
@@ -0,0 +1,19 @@
+cd /sources/
+tar xvf attr*.gz
+cd attr*
+
+pkg watch /mnt/lfs
+
+sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in
+./configure --prefix=/usr --bindir=/bin
+
+make
+make -j1 tests root-tests
+
+make install install-dev install-lib
+chmod -v 755 /usr/lib/libattr.so
+
+mv -v /usr/lib/libattr.so.* /lib
+ln -sfv ../../lib/$(readlink /usr/lib/libattr.so) /usr/lib/libattr.so
+
+pkg build /sources/ini/attr.ini winst.log /
