697b1ec6c1f4967af38466025652b305d0514987
wwccss <chunsheng@cnezsoft.com>
2015-02-14 11:09:40 +0800

    * upgrade to 7.6 stable.

--- lfs/01_lfs/03/3.1.2_download_package.sh
+++ lfs/01_lfs/03/3.1.2_download_package.sh
@@ -1,3 +1,3 @@
-sudo wget -c http://www.linuxfromscratch.org/lfs/view/7.6-rc1/wget-list
+sudo wget -c http://www.linuxfromscratch.org/lfs/view/stable/wget-list
 sudo wget -c -i wget-list -P $LFS/sources
 sudo rm -f wget-list
--- lfs/01_lfs/03/3.1.3_md5_checking.sh
+++ lfs/01_lfs/03/3.1.3_md5_checking.sh
@@ -1,4 +1,4 @@
-sudo wget -c  -O $LFS/sources/md5sums http://www.linuxfromscratch.org/lfs/view/7.6-rc1/md5sums
+sudo wget -c  -O $LFS/sources/md5sums http://www.linuxfromscratch.org/lfs/view/stable/md5sums
 pushd $LFS/sources
 md5sum -c md5sums
 popd
