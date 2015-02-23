3a43d7550c1ad79493d141d6e9b7211df665d8c6
wwccss <chunsheng@cnezsoft.com>
2015-02-23 19:17:09 +0800

    * adjust the shadow shell.

--- lfs/01_lfs/06/6.25_build_shadow.sh
+++ lfs/01_lfs/06/6.25_build_shadow.sh
@@ -1,6 +1,8 @@
 cd /sources/
-tar jxvf shadow*.bz2
+tar xvf shadow*.xz
 cd shadow*
+
+pkg watch /mnt/lfs
 
 sed -i 's/groups$(EXEEXT) //' src/Makefile.in
 find man -name Makefile.in -exec sed -i 's/groups\.1 / /' {} \;
@@ -11,7 +13,9 @@ sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' \
 sed -i 's@DICTPATH.*@DICTPATH\t/lib/cracklib/pw_dict@' \
     etc/login.defs
 
-./configure --sysconfdir=/etc
+sed -i 's/1000/999/' etc/useradd
+
+./configure --sysconfdir=/etc --with-group-name-max-length=32
 make
 make install
 mv -v /usr/bin/passwd /bin
@@ -21,3 +25,5 @@ grpconv
 sed -i 's/yes/no/' /etc/default/useradd
 
 passwd root
+
+pkg savelog shadow
