50f2ab478701e018cfc48fdb1f1ec7f2d39d58e7
wwccss <chunsheng@cnezsoft.com>
2015-02-23 19:32:30 +0800

    * adjust build shell for procps-ng package.

--- lfs/01_lfs/06/6.27_build_procps_ng.sh
+++ lfs/01_lfs/06/6.27_build_procps_ng.sh
@@ -2,25 +2,20 @@ cd /sources/
 tar xvf procps-ng*.xz 
 cd procps*
 
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr                           \
             --exec-prefix=                          \
             --libdir=/usr/lib                       \
-            --docdir=/usr/share/doc/procps-ng-3.3.6 \
+            --docdir=/usr/share/doc/procps-ng-3.3.9 \
             --disable-static                        \
-            --disable-skill                         \
             --disable-kill
 
 make
-
-pushd testsuite
-  sed -i -e 's|exec which sleep|exec echo /tools/bin/sleep|' \
-         -e 's|999999|&9|'                       config/unix.exp
-  sed -i -e 's|pmap_initname\\\$|pmap_initname|' pmap.test/pmap.exp
-  make site.exp
-  DEJAGNU=global-conf.exp runtest
-popd
-
 make install
 
+mv -v /usr/bin/pidof /bin
 mv -v /usr/lib/libprocps.so.* /lib
-ln -sfv ../../lib/libprocps.so.1.1.0 /usr/lib/libprocps.so
+ln -sfv ../../lib/$(readlink /usr/lib/libprocps.so) /usr/lib/libprocps.so
+
+pkg savelog procps-ng
