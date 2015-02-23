7bffa7a43ffb17a3e42e8ddf707296ed9345a7ee
wwccss <chunsheng@cnezsoft.com>
2015-02-23 19:25:12 +0800

    * adjust the build shell of psmisc.

--- lfs/01_lfs/06/6.26_build_psmisc.sh
+++ lfs/01_lfs/06/6.26_build_psmisc.sh
@@ -2,8 +2,12 @@ cd /sources/
 tar xvf psmisc*.gz
 cd psmisc*
 
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr
 make
 make install
 mv -v /usr/bin/fuser   /bin
 mv -v /usr/bin/killall /bin
+
+pkg savelog psmisc
