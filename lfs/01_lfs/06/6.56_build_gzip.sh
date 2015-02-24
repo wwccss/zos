fd626a856fc2be10b0ec8e459d720db5882dce05
wwccss <chunsheng@cnezsoft.com>
2015-02-24 09:21:45 +0800

    * adjust gzip package.

--- lfs/01_lfs/06/6.56_build_gzip.sh
+++ lfs/01_lfs/06/6.56_build_gzip.sh
@@ -2,10 +2,13 @@ cd /sources/
 tar xvf gzip*.xz
 cd gzip*
 
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr --bindir=/bin
 make
-make check
 make install
 
 mv -v /bin/{gzexe,uncompress,zcmp,zdiff,zegrep} /usr/bin
 mv -v /bin/{zfgrep,zforce,zgrep,zless,zmore,znew} /usr/bin
+
+pkg savelog gzip
