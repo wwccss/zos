609622d66be6f17fd0355ac1bb18dc433429ea03
wwccss <chunsheng@cnezsoft.com>
2015-02-24 13:52:33 +0800

    * the grub config package.

--- lfs/01_lfs/08/8.04_install_grub.sh
+++ lfs/01_lfs/08/8.04_install_grub.sh
@@ -1,13 +1,14 @@
+cd /tmp/
+pkg watch /mnt/lfs
 grub-install /dev/sdb
 cat > /boot/grub/grub.cfg << "EOF"
-# Begin /boot/grub/grub.cfg
 set default=0
 set timeout=5
-
 insmod ext2
 set root=(hd0,1)
-
-menuentry "zentao management operating system 1.0" {
-        linux   /boot/vmlinuz-3.8.1-lfs-7.3 root=/dev/sda1 ro
+menuentry "zos 1.1(build from LFS 7.6)" {
+        linux /boot/vmlinuz-3.16.2-lfs-7.6 root=/dev/sda1 ro
 }
 EOF
+
+pkg savelog grub-config
