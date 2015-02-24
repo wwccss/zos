ff54b2cd48d7f59daec426245c8e28cf7986ba1f
wwccss <chunsheng@cnezsoft.com>
2015-02-24 09:35:07 +0800

    * adjust libpipeline package.

--- lfs/01_lfs/06/6.60_build_libpipeline.sh
+++ lfs/01_lfs/06/6.60_build_libpipeline.sh
@@ -2,8 +2,11 @@ cd /sources/
 tar xvf libpipeline*.gz
 cd libpi*
 
+pkg watch /mnt/lfs
+
 PKG_CONFIG_PATH=/tools/lib/pkgconfig ./configure --prefix=/usr
 
 make
-make check
 make install
+
+pkg savelog libpipeline
