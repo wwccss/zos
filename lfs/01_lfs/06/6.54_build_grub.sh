e7dd56efb0493879703b8859288611f8433617b2
wwccss <chunsheng@cnezsoft.com>
2015-02-24 09:11:26 +0800

    * adjust grub package.

--- lfs/01_lfs/06/6.54_build_grub.sh
+++ lfs/01_lfs/06/6.54_build_grub.sh
@@ -2,13 +2,16 @@ cd /sources/
 tar xvf grub*.xz
 cd grub*
 
-sed -i -e '/gets is a/d' grub-core/gnulib/stdio.in.h
+pkg watch /mnt/lfs
 
+sed -i -e '/gets is a/d' grub-core/gnulib/stdio.in.h
 ./configure --prefix=/usr          \
+            --sbindir=/sbin        \
             --sysconfdir=/etc      \
             --disable-grub-emu-usb \
             --disable-efiemu       \
             --disable-werror
-
 make
 make install
+
+pkg savelog grub
