8dcc1f415cd2760ad20d196afe58dc7950d3e634
wwccss <chunsheng@cnezsoft.com>
2015-02-24 08:59:31 +0800

    * adjust groff package.

--- lfs/01_lfs/06/6.52_build_groff.sh
+++ lfs/01_lfs/06/6.52_build_groff.sh
@@ -2,13 +2,12 @@ cd /sources/
 tar xvf groff*.gz
 cd groff*
 
+pkg watch /mnt/lfs
+
 # set page size to A4 default.
 PAGE=A4 ./configure --prefix=/usr
 
 make
-
-mkdir -p /usr/share/doc/groff-1.22/pdf
 make install
 
-ln -sv eqn /usr/bin/geqn
-ln -sv tbl /usr/bin/gtbl
+pkg savelog groff
