f5534c2272d746c278ae84218d73b2d3bc9bbb32
wwccss <chunsheng@cnezsoft.com>
2015-02-26 19:52:56 +0800

    * upgrade to 7.6.

--- lfs/02_blfs/01_basic/1.1_prepare/02_down_blfs_boot_scripts.sh
+++ lfs/02_blfs/01_basic/1.1_prepare/02_down_blfs_boot_scripts.sh
@@ -1,5 +1,5 @@
 sudo mkdir /sources/blfs
 sudo chown 1000:1000 /sources/blfs
 cd /sources/blfs
-wget http://www.linuxfromscratch.org/blfs/downloads/svn/blfs-bootscripts-20130725.tar.bz2
+wget http://www.linuxfromscratch.org/blfs/downloads/7.6/blfs-bootscripts-20140919.tar.bz2
 tar xvf blfs-bootscripts*.tar.bz2
