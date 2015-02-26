2ea02ed7bc6e559199cddcfdd9197f7f23a99cd5
wwccss <chunsheng@cnezsoft.com>
2015-02-26 22:17:34 +0800

    + add the shell to build openssl.

--- lfs/02_blfs/01_basic/1.2.1_build_openssl.sh
+++ lfs/02_blfs/01_basic/1.2.1_build_openssl.sh
@@ -2,17 +2,18 @@ cd /sources/blfs
 tar xvf openssl*.tar.gz
 cd openssl*
 
-patch -Np1 -i ../openssl-1.0.1e-fix_parallel_build-1.patch &&
-patch -Np1 -i ../openssl-1.0.1e-fix_pod_syntax-1.patch &&
+pkg watch /mnt/lfs
+
+patch -Np1 -i ../openssl-1.0.1i-fix_parallel_build-1.patch &&
 
 ./config --prefix=/usr         \
          --openssldir=/etc/ssl \
+         --libdir=lib          \
          shared                \
          zlib-dynamic &&
 make
 
-make test
-
 make MANDIR=/usr/share/man MANSUFFIX=ssl install &&
-install -dv -m755 /usr/share/doc/openssl-1.0.1e  &&
-cp -vfr doc/*     /usr/share/doc/openssl-1.0.1e
+install -dv -m755 /usr/share/doc/openssl-1.0.1i  &&
+
+pkg savelog openssl
