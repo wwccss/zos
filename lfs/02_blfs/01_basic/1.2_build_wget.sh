3b932b8f107f9727a2b0918ebf61c727e27963ab
wwccss <chunsheng@cnezsoft.com>
2015-02-26 22:24:44 +0800

    + add the wget shell.

--- lfs/02_blfs/01_basic/1.2_build_wget.sh
+++ lfs/02_blfs/01_basic/1.2_build_wget.sh
@@ -2,11 +2,14 @@ cd /sources/blfs
 tar xvf wget*.tar.xz
 cd wget*
 
-patch -Np1 -i ../wget-1.14-texi2pod-1.patch &&
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr      \
             --sysconfdir=/etc  \
             --with-ssl=openssl &&
 make 
-make check
+make install
 
-sudo make install
+echo ca-directory=/etc/ssl/certs >> /etc/wgetrc
+
+pkg savelog wget
