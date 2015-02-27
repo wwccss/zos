b07d5bed937a477789558120bb8f5c042f8088c4
wwccss <chunsheng@cnezsoft.com>
2015-02-27 22:56:15 +0800

    + add cron package.

--- lfs/02_blfs/01_basic/1.06_build_cron.sh
+++ lfs/02_blfs/01_basic/1.06_build_cron.sh
@@ -1,26 +1,30 @@
-sudo groupadd -g 24 vxicron &&
-sudo useradd -c "Vixie cron User" -d /dev/null -u 24 -g vxicron -s /bin/false vxicron
+groupadd -g 25 vxicron &&
+useradd -c "Vixie cron User" -d /dev/null -u 25 -g vxicron -s /bin/false vxicron
 
 cd /sources/blfs
 mkdir cron_4.1
 cd cron_4.1
 sh < ../cron*.shar
 
+pkg watch /mnt/lfs
+
 sed -i 's@/usr/ucb/vi@/usr/bin/vim@' pathnames.h &&
 echo '#define CRON_GROUP "vxicron"' >> config.h &&
 make
-
-sudo make install
+make install
 
 cd ../blfs-bootscript*
 sed -i 's/fcron/cron/g' blfs/init.d/fcron
 mv blfs/init.d/fcron blfs/init.d/cron
 sed -i 's/fcron/cron/g' Makefile
-sudo make install-cron
+make install-cron
 
-sudo mkdir /var/cron
-sudo touch /var/cron/cron.allow
-sudo touch /var/cron/cron.deny
-sudo chown z:z /var/cron/cron.allow
+mkdir /var/cron
+touch /var/cron/cron.allow
+touch /var/cron/cron.deny
+chown z:z /var/cron/cron.allow
 echo z > /var/cron/cron.allow
-sudo chown root:root /var/cron/cron.allow
+chown root:root /var/cron/cron.allow
+
+cd -
+pkg savelog vixiecron
