6bac9d27d4a902dc72ba9b83e9ecfe04b89ae8c5
wwccss <chunsheng@cnezsoft.com>
2015-02-27 20:19:52 +0800

    * init /dev/ before chroot.

--- lfs/02_blfs/00_prepare/06_chroot.sh
+++ lfs/02_blfs/00_prepare/06_chroot.sh
@@ -1,3 +1,15 @@
+sudo mkdir -pv $LFS/{dev,proc,sys,run}
+sudo mknod -m 600 $LFS/dev/console c 5 1
+sudo mknod -m 666 $LFS/dev/null c 1 3
+sudo mount -v --bind /dev $LFS/dev
+sudo mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+sudo mount -vt proc proc $LFS/proc
+sudo mount -vt sysfs sysfs $LFS/sys
+sudo mount -vt tmpfs tmpfs $LFS/run
+if [ -h $LFS/dev/shm ]; then
+  sudo mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+fi
+
 # this is run out chroot.
 sudo chroot "$LFS" /usr/bin/env -i              \
   HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
