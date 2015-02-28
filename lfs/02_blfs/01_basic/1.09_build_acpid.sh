755b3d80859eeab8aa28112d5c7322d91b538b42
wwccss <chunsheng@cnezsoft.com>
2015-02-28 14:25:33 +0800

    * acpid package.

--- lfs/02_blfs/01_basic/1.09_build_acpid.sh
+++ lfs/02_blfs/01_basic/1.09_build_acpid.sh
@@ -2,27 +2,31 @@ cd /sources/blfs
 tar xvf acpid*.tar.xz
 cd acpid*
 
-./configure --prefix=/usr --docdir=/usr/share/doc/acpid-2.0.19 &&
+pkg watch /mnt/lfs
+
+./configure --prefix=/usr --docdir=/usr/share/doc/acpid-2.0.23 &&
 make
 
-sudo make install &&
-sudo install -v -m755 -d /etc/acpi/events &&
-sudo cp -r samples /usr/share/doc/acpid-2.0.19
+make install &&
+install -v -m755 -d /etc/acpi/events
 
 cat > power << "EOF"
 event=button/power
 action=/etc/acpi/power.sh
 EOF
-sudo mv power /etc/acpi/events/
+mv power /etc/acpi/events/
 
 cat > power.sh << "EOF"
 #!/bin/sh
 /sbin/shutdown -h now "Power button pressed"
 EOF
 
-sudo mv power.sh /etc/acpi/
+mv power.sh /etc/acpi/
 
-sudo chmod +x /etc/acpi/power.sh
+chmod +x /etc/acpi/power.sh
 
 cd ../blfs-boot*
-sudo make install-acpid
+make install-acpid
+
+cd -
+pkg savelog acpid
