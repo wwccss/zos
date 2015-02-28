4df5c34f24ec2a218343e227d6d74e344332581d
wwccss <chunsheng@cnezsoft.com>
2015-02-28 16:57:49 +0800

    * pcre package.

--- lfs/02_blfs/03_apache/03_build_pcre.sh
+++ lfs/02_blfs/03_apache/03_build_pcre.sh
@@ -2,18 +2,21 @@ cd /sources/blfs
 tar xvf pcre*.tar.bz2
 cd pcre*
 
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr                     \
-            --docdir=/usr/share/doc/pcre-8.33 \
-            --enable-utf                      \
+            --docdir=/usr/share/doc/pcre-8.35 \
             --enable-unicode-properties       \
+            --enable-pcre16                   \
+            --enable-pcre32                   \
             --enable-pcregrep-libz            \
             --enable-pcregrep-libbz2          \
             --enable-pcretest-libreadline     \
             --disable-static                 &&
 make
 
-make test
+make install                     &&
+mv -v /usr/lib/libpcre.so.* /lib &&
+ln -sfv ../../lib/$(readlink /usr/lib/libpcre.so) /usr/lib/libpcre.so
 
-sudo make install                     &&
-sudo mv -v /usr/lib/libpcre.so.* /lib &&
-sudo ln -sfv ../../lib/libpcre.so.1.2.1 /usr/lib/libpcre.so
+pkg savelog pcre
