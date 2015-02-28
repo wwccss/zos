0c5e8a8535f53669278116e1dc95650c1ec41729
wwccss <chunsheng@cnezsoft.com>
2015-02-28 16:43:31 +0800

    * apr-util package.

--- lfs/02_blfs/03_apache/02_build_apr_util.sh
+++ lfs/02_blfs/03_apache/02_build_apr_util.sh
@@ -2,13 +2,14 @@ cd /sources/blfs
 tar xvf apr-util*.tar.bz2
 cd apr-uti*
 
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr       \
             --with-apr=/usr     \
             --with-gdbm=/usr    \
             --with-openssl=/usr \
             --with-crypto &&
 make
-make test
-sudo make install
+make install
 
-
+pkg savelog apr-util
