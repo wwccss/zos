5340eedda089089c860af9c2557a4077f5779312
wwccss <chunsheng@cnezsoft.com>
2015-03-01 16:12:07 +0800

    * adjust libgd package.

--- lfs/02_blfs/05_php/01_gd/08_build_gd.sh
+++ lfs/02_blfs/05_php/01_gd/08_build_gd.sh
@@ -1,6 +1,8 @@
 cd /sources/blfs
-tar xvf libgd*.tar.gz
+tar xvf libgd*.tar.xz
 cd libgd*
+
+pkg watch /mnt/lfs
 
 ./configure --prefix=/usr/ --with-png=/usr/ --with-freetype=/usr/ --with-fontconfig=/usr --with-jpeg=/usr --with-tiff=/usr
 make
