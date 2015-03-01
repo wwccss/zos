2ea38c12cf4d9df8cb5beb6272d0bdd51b709344
wwccss <chunsheng@cnezsoft.com>
2015-03-01 12:08:29 +0800

    * rename and delete.

--- lfs/02_blfs/05_php/07_mhash/01_down_mhash.sh
+++
@@ -1,2 +0,0 @@
-cd /sources/blfs
-wget http://sourceforge.net/projects/mhash/files/mhash/0.9.9.9/mhash-0.9.9.9.tar.bz2
--- lfs/02_blfs/05_php/07_mhash/02_build_mhash.sh
+++
@@ -1,8 +0,0 @@
-cd /sources/blfs
-tar xvf mhash*.tar.bz2
-cd mhash*
-
-./configure --prefix=/usr
-make 
-sudo make install
-
