cfa23b1fbe6a9116d33baef051f5a7d059037fbe
wwccss <chunsheng@cnezsoft.com>
2015-03-01 16:22:06 +0800

    * cyrus-sasl package.

--- lfs/02_blfs/02_security/04_build_sasl.sh
+++ lfs/02_blfs/02_security/04_build_sasl.sh
@@ -2,20 +2,17 @@ cd /sources/blfs
 tar xvf cyrus-sasl*.tar.gz
 cd cyrus*
 
-patch -Np1 -i ../cyrus-sasl-2.1.26-fixes-1.patch &&
-autoreconf -fi &&
-pushd saslauthd
-autoreconf -fi &&
-popd
-./configure --prefix=/usr \
-            --sysconfdir=/etc \
-            --with-dbpath=/var/lib/sasl/sasldb2 \
-            --with-saslauthd=/var/run/saslauthd \
-            --enable-auth-sasldb &&
-make
+pkg watch /mnt/lfs
 
-sudo make install &&
-sudo install -v -dm755 /usr/share/doc/cyrus-sasl-2.1.26 &&
-sudo install -v -m644  doc/{*.{html,txt,fig},ONEWS,TODO} \
-    saslauthd/LDAP_SASLAUTHD /usr/share/doc/cyrus-sasl-2.1.26 &&
-sudo install -v -dm700 /var/lib/sasl
+patch -Np1 -i ../cyrus-sasl-2.1.26-fixes-3.patch &&
+autoreconf -fi &&
+./configure --prefix=/usr        \
+            --sysconfdir=/etc    \
+            --enable-auth-sasldb \
+            --with-dbpath=/var/lib/sasl/sasldb2 \
+            --with-saslauthd=/var/run/saslauthd &&
+make
+make install 
+install -v -dm700 /var/lib/sasl
+
+pkg savelog cyrus-sasl
