0b6cd9988f41b6f2899d669f89bed89d9c1162b7
wwccss <chunsheng@cnezsoft.com>
2015-03-01 10:19:14 +0800

    * apache2 pachage.

--- lfs/02_blfs/03_apache/04_build_apache.sh
+++ lfs/02_blfs/03_apache/04_build_apache.sh
@@ -2,32 +2,37 @@ cd /sources/blfs
 tar xvf httpd*.tar.bz2
 cd httpd*
 
-sudo groupadd -g 25 apache &&
-sudo useradd -c "Apache Server" -d /srv/www -g apache \
-        -s /bin/false -u 25 apache
+pkg watch /mnt/lfs
 
-patch -Np1 -i ../httpd-2.4.4-blfs_layout-1.patch &&
-./configure --enable-layout=BLFS \
-            --enable-mods-shared="all cgi" \
-            --enable-mpms-shared=all \
-            --with-apr=/usr/bin/apr-1-config \
-            --with-apr-util=/usr/bin/apu-1-config \
-            --enable-suexec=shared \
-            --with-suexec-bin=/usr/lib/httpd/suexec \
-            --with-suexec-docroot=/srv/www \
-            --with-suexec-caller=apache \
-            --with-suexec-userdir=public_html \
+groupadd -g 26 apache &&
+useradd -c "Apache Server" -d /srv/www -g apache \
+        -s /bin/false -u 26 apache
+
+patch -Np1 -i ../httpd-2.4.10-blfs_layout-1.patch &&
+sed '/dir.*CFG_PREFIX/s@^@#@' -i support/apxs.in &&
+./configure --enable-authnz-fcgi                            \
+            --enable-layout=BLFS                            \
+            --enable-mods-shared="all cgi"                  \
+            --enable-mpms-shared=all                        \
+            --enable-suexec=shared                          \
+            --with-apr=/usr/bin/apr-1-config                \
+            --with-apr-util=/usr/bin/apu-1-config           \
+            --with-suexec-bin=/usr/lib/httpd/suexec         \
+            --with-suexec-caller=apache                     \
+            --with-suexec-docroot=/srv/www                  \
             --with-suexec-logfile=/var/log/httpd/suexec.log \
-            --with-suexec-uidmin=100 &&
-make
+            --with-suexec-uidmin=100                        \
+            --with-suexec-userdir=public_html               &&
+make install                                 &&
 
-sudo make install &&
+mv -v /usr/sbin/suexec /usr/lib/httpd/suexec &&
+chgrp apache           /usr/lib/httpd/suexec &&
+chmod 4754             /usr/lib/httpd/suexec &&
 
-sudo mv -v /usr/sbin/suexec /usr/lib/httpd/suexec &&
-sudo chgrp apache /usr/lib/httpd/suexec &&
-sudo chmod 4754 /usr/lib/httpd/suexec &&
-
-sudo chown -v -R apache:apache /srv/www
+chown -v -R apache:apache /srv/www
 
 cd ../blfs-bootscripts*
-sudo make install-httpd
+make install-httpd
+
+cd -
+pkg savelog apache2
