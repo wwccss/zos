dca8261bb0fc41d10ed58f5fa6a3394c216bcd55
wwccss <chunsheng@cnezsoft.com>
2015-03-01 15:09:19 +0800

    * package of libjpeg-turbo.

--- lfs/02_blfs/05_php/01_gd/02_build_libjpeg.sh
+++ lfs/02_blfs/05_php/01_gd/02_build_libjpeg.sh
@@ -2,12 +2,16 @@ cd /sources/blfs
 tar xvf libjpeg*.tar.gz
 cd libjpeg*
 
-./configure --prefix=/usr \
+pkg watch /mnt/lfs
+
+sed -i -e '/^docdir/     s:$:/libjpeg-turbo-1.3.1:' \
+       -e '/^exampledir/ s:$:/libjpeg-turbo-1.3.1:' Makefile.in &&
+
+./configure --prefix=/usr           \
             --mandir=/usr/share/man \
-            --with-jpeg8 \
+            --with-jpeg8            \
             --disable-static &&
 make
+make install
 
-docsdir=/usr/share/doc/libjpeg-turbo-1.3.0 &&
-sudo make docdir=$docsdir exampledir=$docsdir install &&
-unset docsdir
+pkg savelog libjpeg-turbo
