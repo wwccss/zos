aa08504df8e602cc2ad3066eaf4b9ac5fb38efe1
wwccss <chunsheng@cnezsoft.com>
2015-03-01 16:14:59 +0800

    * rename.

--- lfs/02_blfs/05_php/02_ldap/02_ldap/01_down_ldap.sh
+++
@@ -1,6 +0,0 @@
-cd /sources/blfs
-wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.35.tgz
-wget http://www.linuxfromscratch.org/patches/blfs/svn/openldap-2.4.35-blfs_paths-1.patch
-wget http://www.linuxfromscratch.org/patches/blfs/svn/openldap-2.4.35-symbol_versions-1.patch
-wget http://www.linuxfromscratch.org/patches/blfs/svn/openldap-2.4.35-ntlm-1.patch
-
