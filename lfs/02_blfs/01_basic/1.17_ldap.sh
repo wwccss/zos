42e123bae4daa68a6f8f83bfe3820af5a2556051
wwccss <chunsheng@cnezsoft.com>
2015-03-01 16:36:41 +0800

    * openladp package.

--- lfs/02_blfs/01_basic/1.17_ldap.sh
+++ lfs/02_blfs/01_basic/1.17_ldap.sh
@@ -2,25 +2,19 @@ cd /sources/blfs
 tar xvf openldap*.tgz
 cd openldap*
 
-patch -Np1 -i ../openldap-2.4.35-ntlm-1.patch
-patch -Np1 -i ../openldap-2.4.35-blfs_paths-1.patch &&
-patch -Np1 -i ../openldap-2.4.35-symbol_versions-1.patch &&
+pkg watch /mnt/lfs
+
+patch -Np1 -i ../openldap-2.4.39-blfs_paths-1.patch &&
+patch -Np1 -i ../openldap-2.4.39-symbol_versions-1.patch &&
 autoconf &&
 ./configure --prefix=/usr     \
             --sysconfdir=/etc \
+            --disable-static  \
             --enable-dynamic  \
             --disable-debug   \
             --disable-slapd &&
 make depend &&
 make
+make install
 
-sudo groupadd -g 83 ldap &&
-sudo useradd -c "OpenLDAP Daemon Owner" -d /var/lib/openldap -u 83 \
-        -g ldap -s /bin/false ldap
-
-sudo make install &&
-sudo chown -R ldap:ldap /var/lib/openldap &&
-sudo install -v -dm755  /usr/share/doc/openldap-2.4.35 &&
-sudo cp -vfr doc/drafts /usr/share/doc/openldap-2.4.35 &&
-sudo cp -vfr doc/rfc    /usr/share/doc/openldap-2.4.35 &&
-sudo cp -vfr doc/guide  /usr/share/doc/openldap-2.4.35
+pkg savelog openldap
