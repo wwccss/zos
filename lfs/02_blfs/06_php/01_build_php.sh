b6cc9475cfe1c33e7151df467748c45888a7ce93
wwccss <chunsheng@cnezsoft.com>
2015-03-01 17:50:43 +0800

    * php5.5 package.

--- lfs/02_blfs/06_php/01_build_php.sh
+++ lfs/02_blfs/06_php/01_build_php.sh
@@ -2,6 +2,8 @@ cd /sources/blfs
 tar xvf php*.tar.bz2
 cd php*
 
+pkg watch /mnt/lfs
+
 ./configure --prefix=/usr \
             --sysconfdir=/etc \
             --with-apxs2 \
@@ -22,13 +24,9 @@ cd php*
             --with-mysqli \
             --with-mysql-sock=/var/run/mysqld/mysqld.sock \
             --with-pdo-mysql \
-            --without-pdo-sqlite \
             --with-readline \
             --enable-sockets \
-            --with-tidy \
             --with-iconv \
-	    --with-mhash \
-            --with-mcrypt \
 	    --disable-cgi \
             --with-gd \
             --with-jpeg-dir=/usr/ \
@@ -36,19 +34,12 @@ cd php*
             --with-zlib-dir=/usr/ \
             --with-freetype-dir=/usr/ \
             --enable-gd-native-ttf \
-            --enable-gd-jis-conv 
+            --enable-gd-jis-conv  \
+
 make clean
 make
 
-sudo make install &&
-sudo install -v -m644 php.ini-production /etc/php.ini &&
+make install &&
+install -v -m644 php.ini-production /etc/php.ini
 
-sudo install -v -m755 -d /usr/share/doc/php-5.4.11 &&
-sudo install -v -m644    CODING_STANDARDS EXTENSIONS INSTALL NEWS \
-                    README* UPGRADING* php.gif \
-                    /usr/share/doc/php-5.4.11 &&
-sudo ln -v -sfn          /usr/lib/php/doc/Archive_Tar/docs/Archive_Tar.txt \
-                    /usr/share/doc/php-5.4.11 &&
-sudo ln -v -sfn          /usr/lib/php/doc/Structures_Graph/docs \
-                    /usr/share/doc/php-5.4.11
-
+pkg savelog php55
