b2feb531c365a3d5afe82bcfd655e7332dce89bb
wwccss <chunsheng@cnezsoft.com>
2015-03-03 16:31:13 +0800

    * subversion package.

--- lfs/02_blfs/08_scm/02_build_subversion.sh
+++ lfs/02_blfs/08_scm/02_build_subversion.sh
@@ -1,23 +1,23 @@
 cd /sources/blfs
-#tar xvf subversion*tar.bz2
+tar xvf subversion*tar.bz2
 cd subversion*
+
+pkg watch /mnt/lfs
 
 ./configure --prefix=/usr/ \
 	--disable-static \
 	--with-apxs=/usr/bin/apxs \
 	--with-apache-libexecdir=/usr/lib/httpd/modules/ \
-	--with-neon=/usr/bin/neon-config \
 	--with-apr=/usr/bin/apr-1-config \
 	--with-apr-util=/usr/bin/apu-1-config \
 	--with-sqlite=/usr \
-	--with-berkeley-db=db.h:/usr/include/:/usr/lib/:db-6.0 \
 	--with-sasl=/usr \
 	--with-openssl=/usr \
+        --with-serf=/usr \
 	--with-zlib=/usr 
 make
+make install
+
+pkg savelog subversion
 
 
-sudo make install &&
-sudo install -v -m755 -d /usr/share/doc/subversion-1.8.0 &&
-sudo cp      -v -R       doc/* \
-                    /usr/share/doc/subversion-1.8.0
--- lfs/02_blfs/08_scm/03_subversion/01_down_subversion.sh
+++
@@ -1,2 +0,0 @@
-cd /sources/blfs
-wget http://archive.apache.org/dist/subversion/subversion-1.8.0.tar.bz2
