c5c5dfa09fcd6af9918a201ad3bb8cfd77b526e7
wwccss <chunsheng@cnezsoft.com>
2015-05-19 20:35:53 +0800

    * adjust the profile.

--- 01_lfs/07/7.07_create_profile.sh
+++ 01_lfs/07/7.07_create_profile.sh
@@ -2,4 +2,6 @@ cat > /etc/profile << "EOF"
 export LANG=en_US.UTF-8
 export LC_ALL=C
 export SVN_EDITOR="/usr/bin/vim"
+PROMPT_COMMAND='echo -ne "\033]0;`hostname`:`basename $PWD`\\007"'
+PS1='[\u@\h:\w\$] '
 EOF
