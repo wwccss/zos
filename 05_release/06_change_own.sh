508b9a91f173bd8501820c6752afaad8a67d87dc
wwccss <chunsheng@cnezsoft.com>
2015-05-20 14:29:44 +0800

    * add execute permission.

--- 05_release/06_change_own.sh
+++ 05_release/06_change_own.sh
@@ -1,2 +1,2 @@
 sudo chown -Rv 1000:1000 /tmp/release/home/z
-sudo chown -Rv 1000:1000 /tmp/release/home/srv/{auth,phpmyadmin,www,zentao}
+sudo chown -Rv 1000:1000 /tmp/release/home/srv/{auth,pma,www,zentao,ranzhi,chanzhi}
